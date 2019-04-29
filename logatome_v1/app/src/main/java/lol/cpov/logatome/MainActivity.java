738cd17842a900c463d7a870cae5ddd9d1a59afe
SantaHey <33598903+SantaHey@users.noreply.github.com>
2019-04-29 20:55:13 +0200

    Update application Android

--- logatome_v1/app/src/main/java/lol/cpov/logatome/MainActivity.java
+++ logatome_v1/app/src/main/java/lol/cpov/logatome/MainActivity.java
@@ -1,62 +1,236 @@
 package lol.cpov.logatome;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
-import android.widget.RadioGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.*;
+import org.apache.commons.net.ftp.FTPClient;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import asilupe.Word;
-import lol.cpov.logatome.adapters.LogatomeItemAdapter;
-import lol.cpov.logatome.models.LogatomeItem;
 
-public class MainActivity extends AppCompatActivity {
-    public Button button;
-    ListView logatomeListView;
-    List<LogatomeItem> logatomeItemList;
+public class MainActivity extends AppCompatActivity implements View.OnDragListener, View.OnTouchListener {
+    public TextView textLogatome;
+    public LinearLayout textLogatomeView;
+    public ImageView imgGood;
+    public ImageView imgBad;
+    public Button btnUpload;
+
+    public Toast toast;
+
+    public Word word;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        logatomeListView = findViewById(R.id.logatome_list_view);
 
-        logatomeItemList = new ArrayList<>();
-        button.setOnClickListener(new View.OnClickListener() {
+        imgGood = (ImageView) findViewById(R.id.imgGood);
+        imgBad = (ImageView) findViewById(R.id.imgBad);
+        textLogatome = (TextView) findViewById(R.id.textLogatome);
+        textLogatomeView = (LinearLayout) findViewById(R.id.textLogatomeView);
+        btnUpload = (Button) findViewById(R.id.btnUpload);
+
+        textLogatomeView.setOnDragListener(this);
+
+        imgGood.setOnTouchListener(this);
+        imgBad.setOnTouchListener(this);
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                genererMots();
+                new uploadFile().execute();
             }
         });
 
-
-        genererMots();
+        genererMot();
     }
 
-    public void testRadios() {
-        int nonChecked = 0;
-        for (LogatomeItem item : logatomeItemList ) {
-            if (item.getWord().getMotBien() == null) {
-                nonChecked++;
+    //http://androidsrc.net/android-view-drag-drop-functionality-sample-application/
+
+    @Override
+    public boolean onDrag(View view, DragEvent dragEvent) {
+        View v = (View) dragEvent.getLocalState();
+        if (dragEvent.getAction() == DragEvent.ACTION_DROP) {
+            if (view.getId() == R.id.textLogatomeView) {
+                word.setMotBien(v.getId() == R.id.imgGood);
+                addToFile(exportMot());
+                genererMot();
             }
-            if (nonChecked == 0) {
-                button.setClickable(true);
-            }
+        } else if (dragEvent.getAction() == DragEvent.ACTION_DRAG_ENTERED) {
+            int color = v.getId() == R.id.imgGood ? Color.parseColor("#6AC259"): Color.parseColor("#F05228");
+            textLogatomeView.setBackgroundColor(color);
+
+        } else if (dragEvent.getAction() == DragEvent.ACTION_DRAG_EXITED) {
+            textLogatomeView.setBackgroundColor(Color.TRANSPARENT);
+
+        } else if (dragEvent.getAction() == DragEvent.ACTION_DRAG_ENDED) {
+            textLogatomeView.setBackgroundColor(Color.TRANSPARENT);
+            v.setVisibility(View.VISIBLE);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+            view.startDrag(null, shadowBuilder, view, 0);
+            view.setVisibility(View.INVISIBLE);
+
+            return true;
+        }
+        return false;
+    }
+
+    public void genererMot() {
+        word = new Word();
+        textLogatome.setText(word.getMotComplet());
+    }
+
+    public String exportMot() {
+        String contenu = "";
+        contenu += word.getMotBien().toString() + ",";
+        contenu += word.getMotComplet() + ",";
+        for (String phoneme : word.getPhonemes()) {
+            contenu += phoneme + ",";
+        }
+        contenu += word.getSuffixe() + "\r\n";
+
+        return contenu;
+    }
+
+    String filename = "mots";
+    String extension = ".txt";
+
+    public void addToFile(String contenu) {
+        try {
+            FileOutputStream fOut = openFileOutput(filename + extension, MODE_APPEND);
+            OutputStreamWriter osw = new OutputStreamWriter(fOut);
+
+            osw.write(contenu);
+            osw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    public void genererMots() {
-        button.setClickable(false);
-        logatomeItemList.clear();
 
-        for(int i = 0; i < 3; i++) {
-            logatomeItemList.add(new LogatomeItem(new Word()));
+    private class uploadFile extends AsyncTask {
+
+        @Override
+        protected Object doInBackground(Object... objects) {
+
+            MainActivity.this.runOnUiThread(new Runnable() {
+                public void run() {
+                    toast = Toast.makeText(MainActivity.this, "Tentative d'upload...",
+                            Toast.LENGTH_LONG);
+                    toast.show();
+                    btnUpload.setEnabled(false);
+                    imgGood.setEnabled(false);
+                    imgBad.setEnabled(false);
+                }
+            });
+
+            String resultat = "";
+
+            FTPClient client = new FTPClient();
+            Date date = new Date();
+            String filenameUp;
+            String[] credentials = getCredentials();
+
+            File dir = getFilesDir();
+            File file = new File(dir, filename + extension);
+            try {
+                client.connect(credentials[0]);
+                client.enterLocalPassiveMode();
+                client.login(credentials[2], credentials[3]);
+
+                client.changeWorkingDirectory(credentials[1]);
+
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
+                filenameUp = filename + " " + dateFormat.format(date) + extension;
+
+                FileInputStream inputStream = new FileInputStream(file);
+                boolean done = client.storeFile(filenameUp, inputStream);
+
+                if (done) {
+                    if(checkFileExists(filenameUp, client)) {
+                        file.delete();
+                        addToFile("");
+
+                        resultat = "L'upload a fonctionné";
+                    }
+                }
+
+                client.disconnect();
+
+            } catch (IOException ex) {
+                resultat = "L'upload a échoué";
+                ex.printStackTrace();
+            }
+
+            final String finalResultat = resultat;
+            MainActivity.this.runOnUiThread(new Runnable() {
+                public void run() {
+                    btnUpload.setEnabled(true);
+                    imgGood.setEnabled(true);
+                    imgBad.setEnabled(true);
+                    toast.cancel();
+                    toast = Toast.makeText(MainActivity.this, finalResultat,
+                            Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            });
+            return null;
         }
-        logatomeListView.setAdapter(new LogatomeItemAdapter(this, logatomeItemList, this));
+    }
+
+    public String[] getCredentials(){
+        String[] credentials = {"ftp.exemple.com", "/", "user", "password"};
+        InputStream inputStream;
+        BufferedReader reader;
+
+        inputStream = getResources().openRawResource(R.raw.crendentials);
+
+        reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        try {
+            for (int i = 0; i < credentials.length; i++) {
+                credentials[i] = reader.readLine();
+                Log.d("dfsdf", credentials[i]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return credentials;
+    }
+
+    public static boolean checkFileExists(String filename, FTPClient client) throws IOException {
+        InputStream inputStream = client.retrieveFileStream(filename);
+        int returnCode = client.getReplyCode();
+        return !(inputStream == null || returnCode == 550);
     }
 }
